6fc1c55eb7c8033b8df635dc94ee4232cd8ca1f1
tinnt <tinnt@airtrip-intl.com>
2024-09-30 13:47:41 +0700

    edit host

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	http.HandleFunc("/", checkStatus)
 	// Start the HTTP server
 	fmt.Println("Starting server on :8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
 }
 
 func checkStatus(w http.ResponseWriter, r *http.Request) {
