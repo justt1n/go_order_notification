e04b081b1e0023ddbaf4df74b4219f8d5d7f80f5
justt1n <tinnt0709@gmail.com>
2024-10-01 15:24:39 +0700

    prevent connect

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 
 	// Start the HTTP server
 	fmt.Println("Starting server on :8080...")
-	log.Fatal(http.ListenAndServe("0.0.0.0:8080", loggedMux))
+	log.Fatal(http.ListenAndServe("127.0.0.1:8080", loggedMux))
 }
 
 // Health check handler
